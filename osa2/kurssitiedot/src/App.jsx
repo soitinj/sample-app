f51e0e71881d21a8fa70d6691178d623a87673af
Jaakko <jaakko.soitinaho@veikkaus.fi>
2024-03-29 17:06:29 +0200

    Move components to their own modules

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -1,46 +1,5 @@
-const Header = ({name}) => {
-  return (
-    <h1>{name}</h1>
-  )
-}
-
-const Course = ({course}) => {
-  return (
-    <>
-      <CourseHeader name={course.name}></CourseHeader>
-      <Content parts={course.parts}></Content>
-    </>
-  )
-}
-
-const CourseHeader = ({name}) => {
-  return (
-    <h2>{name}</h2>
-  )
-}
-
-const Content = ({parts}) => {
-  return (
-    <>
-      {parts.map((part) => (
-        <Part key={part.id} name={part.name} exercises={part.exercises}></Part>
-      ))}
-      <Total parts={parts}></Total>
-    </>
-  )
-}
-
-const Part = ({name, exercises}) => {
-  return (
-    <p>{name} {exercises}</p>
-  )
-}
-
-const Total = ({parts}) => {
-  return (
-    <b>total of {parts.reduce((acc, part) => acc + part.exercises, 0)} exercises</b>
-  )
-}
+import Header from './components/Header'
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
