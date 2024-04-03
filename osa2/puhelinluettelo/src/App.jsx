f3dc9f9899c2153ffc1415aa8328eefea2e7b84f
Jaakko <jaakko.soitinaho@veikkaus.fi>
2024-04-03 18:49:19 +0300

    Added (some) styling and error handling. Submissions for 2.16-2.17

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -3,12 +3,17 @@ import phoneService from './services/phonebook'
 import PersonList from './components/PersonList'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
+import Notification from './components/Notification'
 
 const App = () => {
+  const defaultNotification = {message: '', success: true}
   const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterName, setFilterName] = useState('')
+  const [notification, setNotification] = useState(defaultNotification)
+
+  const setDefaultNote = () => setTimeout(() => setNotification(defaultNotification), 5000)
 
   useEffect(() => {
     phoneService.getAll()
@@ -21,14 +26,21 @@ const App = () => {
     event.preventDefault()
     const oldPerson = persons.find(p => p.name === newName)
     if (newName === '' || newNumber === '') {
-      alert("Will not add person with empty name or number")
+      setNotification(defaultNotification)
+      setDefaultNote()
     } else if (oldPerson !== undefined) {
-        if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
-          phoneService.update({...oldPerson, number: newNumber}).then(() =>
-            phoneService.getAll().then(response =>
-              setPersons(response.data)
-            )
+      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+        phoneService.update({...oldPerson, number: newNumber}).then(() => {
+          phoneService.getAll().then(response =>
+            setPersons(response.data)
           )
+          setNotification({message: `Updated ${newName}'s phone number!`, success: true})
+          setDefaultNote()
+        }).catch(error => {
+          if (error.response.status === 404) setNotification({message: `Unable to find user ${newName} to update`, success: false})
+          else setNotification({message: `Request failed with code ${error.response.status}`, success: false})
+          setDefaultNote()
+        })
       }
     } else {
       const newPerson = {
@@ -40,6 +52,11 @@ const App = () => {
           setPersons(persons.concat([response]))
           setNewName('')
           setNewNumber('')
+          setNotification({message: `Added ${newName}!`, success: true})
+          setDefaultNote()
+        }).catch(error => {
+          setNotification({message: `Adding ${newName} failed with code ${error.response.status} (was user already added?)`, success: false})
+          setDefaultNote()
         })
     }
   }
@@ -59,6 +76,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification notification={notification}></Notification>
       <Filter filterHandler={handleFilterChange} filterName={filterName} />
       <h2>Add a new person</h2>
       <PersonForm
