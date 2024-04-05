7da39081694a6d215552b52f3546ddb35241a8c1
Jaakko <jaakko.soitinaho@veikkaus.fi>
2024-04-05 21:53:49 +0300

    Fix baseUrl

--- osa2/maatiedot/src/services/countries.js
+++ osa2/maatiedot/src/services/countries.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/'
+const baseUrl = 'https://studies.cs.helsinki.fi/restcountries'
 
 const getAll = () => {
   return axios.get(`${baseUrl}/api/all`)
