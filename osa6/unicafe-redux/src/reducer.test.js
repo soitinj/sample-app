098a4660a4fe9908460bd1a294e698a61d794455
Jaakko <jaakko.soitinaho@veikkaus.fi>
2024-05-29 19:51:38 +0300

    Exercises 6.1-2

--- osa6/unicafe-redux/src/reducer.test.js
+++ osa6/unicafe-redux/src/reducer.test.js
@@ -1,5 +1,6 @@
 import deepFreeze from 'deep-freeze'
 import counterReducer from './reducer'
+import { createStore } from 'redux'
 
 describe('unicafe reducer', () => {
   const initialState = {
@@ -32,4 +33,71 @@ describe('unicafe reducer', () => {
       bad: 0
     })
   })
+
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('stats can be reset', () => {
+    const action = {
+      type: 'ZERO'
+    }
+    const state = {
+      good: 2,
+      ok: 2,
+      bad: 3
+    }
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 0
+    })
+  })
+
+  test('events can be dispatched', () => {
+    const store = createStore(counterReducer)
+
+    store.dispatch({ type: 'BAD' })
+    expect(store.getState()).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+    store.dispatch({ type: 'OK' })
+    expect(store.getState()).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 1
+    })
+  })
+
 })
