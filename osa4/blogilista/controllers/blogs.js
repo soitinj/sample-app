11275f0d011429cd3ea29714a3d9725da9575259
Jaakko <jaakko.soitinaho@veikkaus.fi>
2024-05-29 16:27:09 +0300

    Add endpoint to post a like

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -34,7 +34,20 @@ blogsRouter.delete('/:id', async (request, response) => {
 })
 
 blogsRouter.put('/:id', async (request, response) => {
-  await Blog.findByIdAndUpdate(request.params.id, { likes: request.body.likes }, { new: true, runValidators: true, context: 'query' })
+  await Blog.findByIdAndUpdate(request.params.id, {
+      title: request.body.title,
+      author: request.body.author,
+      url: request.body.url,
+      likes: request.body.likes,
+      user: request.user.id
+    },
+    { new: true, runValidators: true, context: 'query' }
+  )
+  response.status(204).end()
+})
+
+blogsRouter.post('/:id/like', async (request, response) => {
+  await Blog.findByIdAndUpdate(request.params.id, { $inc: { likes: 1 } }, { new: true, runValidators: true, context: 'query' })
   response.status(204).end()
 })
 
