4e93dd367e5d2e2e2c39c94f3dc55608aee664af
Jaakko <jaakko.soitinaho@veikkaus.fi>
2024-09-13 13:10:10 +0300

    Minor touches

--- osa5/bloglist-frontend/src/components/Header.jsx
+++ osa5/bloglist-frontend/src/components/Header.jsx
@@ -1,5 +1,5 @@
 const Header = ({ text, top }) => {
-  return top ? <h1 className='m-4'>{text}</h1> : <h2>{text}</h2>
+  return top ? <h1 className='ms-4'>{text}</h1> : <h2>{text}</h2>
 }
 
 export default Header
