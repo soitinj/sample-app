7b112a5452d98b1f2f569d6e046bc49d16e429a7
Jaakko Soitinaho <jaakko.soitinaho@aalto.fi>
2024-09-14 14:39:37 +0300

    Add basic router

--- osa5/bloglist-frontend/src/main.jsx
+++ osa5/bloglist-frontend/src/main.jsx
@@ -2,5 +2,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router}></RouterProvider>
+)
