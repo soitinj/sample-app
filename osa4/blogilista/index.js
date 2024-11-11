f9877d146e58b7858b744c60b7a6f7c9ef65a82a
Jaakko Soitinaho <jaakko.soitinaho@aalto.fi>
2024-11-11 22:18:43 +0200

    Allow exposing HOST

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -2,6 +2,6 @@ const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
+app.listen(config.PORT, config.HOST, () => {
+  logger.info(`Server running on port ${config.PORT} / host ${config.HOST}`)
 })
