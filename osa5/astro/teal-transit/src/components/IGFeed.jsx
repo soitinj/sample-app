b01ad821a62a8bf80fb0949c8dc54d2ac5870f49
Jaakko Soitinaho <jaakko.soitinaho@aalto.fi>
2024-11-12 19:37:50 +0200

    Expand on comments

--- osa5/astro/teal-transit/src/components/IGFeed.jsx
+++ osa5/astro/teal-transit/src/components/IGFeed.jsx
@@ -1,11 +1,12 @@
 // This component conflicts with SSR, if content is loaded on the server and then passed through to client with
 // <script> tags for instance.
+// The resulting error on the browser: "Prop `id` did not match."
 // This is due to react-social-media-embed creating IDs for components randomly,
 // so prerendered HTML and client hydration clash due to different component ID attributes.
 // https://github.com/justinmahar/react-social-media-embed/blob/master/src/components/embeds/InstagramEmbed.tsx
 // line 65: const uuidRef = React.useRef(generateUUID()); // this is the problematic line
 // If ref is generated in a predicatable fashion, hydration does not crash.
-// Workaround for this project: const uuidRef = React.useRef(() => url) // use url as ref
+// Workaround for this project: const uuidRef = React.useRef(() => (url)) // use url as ref
 import { InstagramEmbed } from 'react-social-media-embed'
 
 const IGFeed = ({ postIds }) => {
